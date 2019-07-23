dbc82e01c890ab7bba41d502522896bc823f45de
luo0412 <1095847440@qq.com>
2019-07-23 20:37:29 +0800

    small change

--- _renren-fast/jdk8-boot2-mp3-hutool-oauth2-jwt-renrenfast-20190704-api/src/main/java/io/renren/config/KaptchaConfig.java
+++ _renren-fast/jdk8-boot2-mp3-hutool-oauth2-jwt-renrenfast-20190704-api/src/main/java/io/renren/config/KaptchaConfig.java
@@ -28,8 +28,9 @@ public class KaptchaConfig {
     public DefaultKaptcha producer() {
         Properties properties = new Properties();
         properties.put("kaptcha.border", "no");
-        properties.put("kaptcha.textproducer.font.color", "black");
-        properties.put("kaptcha.textproducer.char.space", "5");
+        properties.put("kaptcha.textproducer.font.color", "blue");
+        properties.put("kaptcha.textproducer.char.space", "4");
+        properties.put("kaptcha.textproducer.char.length", "4");
         properties.put("kaptcha.textproducer.font.names", "Arial,Courier,cmr10,宋体,楷体,微软雅黑");
         Config config = new Config(properties);
         DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
