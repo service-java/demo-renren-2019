4768ba17ac7968c841e8fbd9cae1670a73ab0323
luo0412 <1095847440@qq.com>
2020-06-04 17:26:46 +0800

    small

--- _renren-fast/webpack3-gulp3-vue-element-renren-ui-2019/config/dev.env.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  // 是否开启代理, 重置后需重启vue-cli
-  OPEN_PROXY: false
-})
